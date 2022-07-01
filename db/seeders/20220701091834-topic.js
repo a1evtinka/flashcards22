116a6cc9f93c412765ba67d88bdbce47624b5c6f
Alevtina <a1evtina@list.ru>
2022-07-01 14:47:42 +0300

    quest done

--- db/seeders/20220701091834-topic.js
+++ db/seeders/20220701091834-topic.js
@@ -13,6 +13,11 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+      {
+        title: 'бонус',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ], {});
   },
 

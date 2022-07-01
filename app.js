0f26b305cd933f2c89822ae1cdb9af9e967c6330
tipsun91 <tipsun911@gmail.com>
2022-07-01 14:09:37 +0300

    Bug fixes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT ?? 3000;
 
 // Импорт роутеров
 const authRouter = require('./routes/auth');
-const mainRouter = require('/routes/index');
+const mainRouter = require('./routes/index');
 
 const app = express();
 
